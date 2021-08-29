bf0e15253d724be37a58eca83add7ca4ddbc0041
Тимур Уразаев <astominsflavadept@gmail.com>
2021-08-29 16:04:32 +0500

    Это моя сборка для пет-проектов.

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 1333))
-server.listen(4)
-print('Waiting...')
-clientSocket, address = server.accept()
-data = clientSocket.recv(1024).decode('utf-8')
-print(data)
-HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-content = 'Well done.'.encode('utf-8')
-clientSocket.send(HDRS.encode('utf-8') + content)
-print('No trains are currently in service')
+def serverStart():
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('127.0.0.1', 1333))
+        server.listen(4)
+        while True:
+            print('Waiting...')
+            clientSocket, address = server.accept()
+            data = clientSocket.recv(1024).decode('utf-8')
+            # print(data)
+            content = loadPage__get(data)
+            clientSocket.send(content)
+            clientSocket.shutdown(socket.SHUT_WR)
+    except KeyboardInterrupt:
+        server.close()
+        print('Shutdown')
+
+def loadPage__get(requestData):
+    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    path = requestData.split(' ')[1]
+    response = ''
+    try:
+        with open('dist'+path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf-8') + response
+    except FileNotFoundError:
+        return (HDRS_404 + "Page not found :/").encode('utf-8')
+
+
+if __name__ == '__main__':
+    serverStart()
